d311f7b54257a289e7d6414b55305299a0e4dcea
Patryk Grandt <spav@spav.com.pl>
2017-04-22 12:11:56 +0200

    basic mechanics

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public Camera cam;
+
+    public GameObject worldPrefab;
+    public GameObject characterPrefab;
+
+    public float rotationSpeed = 2f;
+    public float rotationStopTime = 0.5f;
+    private float currentRotationSpeed = 0;
+    private float rotationVelocity = 0;
+
+    private GameObject world;
+
+    void Start() {
+        world = Instantiate(worldPrefab);
+    }
+
+    void Update() {
+        if(Input.GetButtonDown("Fire2")) {
+            SpawnCharacter();
+        }
+        UpdateWorldRotationSpeed();
+    }
+
+    void FixedUpdate() {
+        ApplyRotationSpeed();
+    }
+
+    private void UpdateWorldRotationSpeed() {
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition); 
+        float rotation = 0;
+        if(Input.GetButton("Fire1")) {
+            float h = rotationSpeed * Input.GetAxis("Mouse X");
+            float v = rotationSpeed * Input.GetAxis("Mouse Y");
+            float x = mouseWorldPos.x;
+            float y = mouseWorldPos.y;
+
+            rotation += x > 0 ? v : -v;
+            rotation += y > 0 ? -h : h;
+            rotation = Mathf.Clamp(rotation, -rotationSpeed, rotationSpeed);
+        } else if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            float h = rotationSpeed * Input.GetAxis("Horizontal");
+            float v = rotationSpeed * Input.GetAxis("Vertical");
+            rotation = v - h;
+            rotation = Mathf.Clamp(rotation, -rotationSpeed, rotationSpeed);
+        } else if(currentRotationSpeed != 0) {
+            rotation = 0;
+        }
+
+        if(currentRotationSpeed != rotation) {
+            currentRotationSpeed = Mathf.SmoothDamp(currentRotationSpeed, rotation, ref rotationVelocity, rotationStopTime);    
+        }
+    }
+
+    private void ApplyRotationSpeed() {
+        if(currentRotationSpeed != 0) {
+            world.transform.Rotate(0, 0, currentRotationSpeed);    
+        }
+    }
+
+    private void SpawnCharacter() {
+        Instantiate(characterPrefab);
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(Collider2D))]
+public class Room : MonoBehaviour {
+
+    public enum State {
+        Cloner,
+        Destroyer
+    }
+
+    public ContactFilter2D contactFilter;
+    public int maxAffected = 10;
+    [Tooltip("In seconds")]
+    public float affectCooldown = 1f;
+    [Tooltip("In seconds")]
+    public float morphCooldown = 5f;
+
+    public Color clonerColor;
+    public Color destroyerColor;
+
+    public Renderer backgroundRenderer1;
+    public Renderer backgroundRenderer2;
+
+    private Collider2D collider2d;
+
+    private float affectCooldownCounter;
+    private float morphCooldownCounter;
+
+    public State state;
+
+    void Awake() {
+        collider2d = GetComponent<Collider2D>();
+    }
+
+    void Update() {
+        UpdateAffect();
+        UpdateMorph();
+        UpdateColor();
+    }
+
+    private void UpdateAffect() {
+        affectCooldownCounter = Mathf.MoveTowards(affectCooldownCounter, 0, Time.deltaTime);
+
+        if(affectCooldownCounter == 0) {
+            AffectArea();
+            affectCooldownCounter = affectCooldown;
+        }
+    }
+
+    private void AffectArea() {
+        Collider2D[] inArea = new Collider2D[maxAffected];
+        int affectedCount = collider2d.OverlapCollider(contactFilter, inArea);
+
+        for(int i = 0; i < affectedCount; i++) {
+            AffectCharacter(inArea[i].gameObject);
+        }
+    }
+
+    private void AffectCharacter(GameObject character) {
+        switch(state) {
+            case State.Cloner:
+                CloneCharacter(character);
+                break;
+            case State.Destroyer:
+                DestroyCharacter(character);
+                break;
+        }
+    }
+
+    private void CloneCharacter(GameObject character) {
+        Instantiate(character);
+    }
+
+    private void DestroyCharacter(GameObject character) {
+        Destroy(character);
+    }
+
+    private void UpdateMorph() {
+        morphCooldownCounter = Mathf.MoveTowards(morphCooldownCounter, 0, Time.deltaTime);
+        if(morphCooldownCounter == 0) {
+            Morph();
+            morphCooldownCounter = morphCooldown;
+        }
+    }
+
+    private void Morph() {
+        state = state == State.Cloner ? State.Destroyer : State.Cloner;
+    }
+
+    private void UpdateColor() {
+        Color color = state == State.Cloner ? clonerColor : destroyerColor;
+        backgroundRenderer1.material.color = color;
+        backgroundRenderer2.material.color = color;
+    }
+}
